65db77acb062d5512baff47adf7658fa4398d443
Paul Fitzgerald <paulfitz99@gmail.com>
2015-03-20 13:15:58 +0000

    thanks gabe

--- lib/big_will.rb
+++ lib/big_will.rb
@@ -1,1 +1,17 @@
 #Your code goes here
+
+def replace(lyrics)
+
+  lyrics = lyrics.gsub(/[will]/, 'w' => '**(w)**', 'i' => '**(i)**', 'l' => '**(l)**')
+
+ puts lyrics
+end
+
+replace("In west Philadelphia born and raised
+On the playground was where I spent most of my days
+Chillin' out maxin' relaxin' all cool
+And all shootin some b-ball outside of the school
+When a couple of guys who were up to no good
+Started making trouble in my neighborhood
+I got in one little fight and my mom got scared
+She said 'You're movin' with your auntie and uncle in Bel Air")
